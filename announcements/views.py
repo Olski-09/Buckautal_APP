cc8094a3dc807801e680b04a5faea0b21e527523
Olski-09 <olitietz2007@gmail.com>
2025-05-05 18:10:21 +0200

    Add announcements app with basic URL and view integration
    Integrated the announcements app by adding a URL pattern to `urlpatterns` in the main project. Created a basic index view, corresponding template, and route for displaying announcements, along with a minor update to the HTML structure.

--- announcements/views.py
+++ announcements/views.py
@@ -1,3 +1,9 @@
+from Scripts.bottle import response
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Post
+from django.template import loader
 # Create your views here.
+def index(request):
+    template = loader.get_template("announcements/index.html")
+    return render(request, "announcements/index.html")
